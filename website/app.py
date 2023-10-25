8106883d54acd025213d39ac9a536a18a64888a8
caiton1 <connor.nicolai.aiton@gmail.com>
2023-10-25 13:26:27 -0700

    updated requirnments

--- website/app.py
+++ website/app.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template
 from firebase import firebase
 
 app = Flask(__name__)
-firebase = firebase.FirebaseApplication()
-
 # Defining the home page of our site
 @app.route("/")  # this sets the route to this page
 def index():
