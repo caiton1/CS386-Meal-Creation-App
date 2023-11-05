de81f3796209f80d7eb2c57be45646ba6f3e33b6
caiton1 <connor.nicolai.aiton@gmail.com>
2023-11-05 01:14:04 -0700

    Added more documentation to user.py and favorite.py

--- website/functions/favorite.py
+++ website/functions/favorite.py
@@ -1,5 +1,7 @@
 '''@author: caiton1'''
-# check if current recipe is favorited 
+# check if current recipe is favorited
+
+
 def is_favorited(user_data, token, selection):
     box_display = ''
     favorite_list = []
@@ -7,8 +9,9 @@ def is_favorited(user_data, token, selection):
     for favorite in user_data.val()[token]['favorites']:
         favorite_list.append(favorite)
         if favorite == selection: 
-            box_display = 'checked' # display checked
+            box_display = 'checked'  # display checked
     return [box_display, favorite_list]
+
     
 # update database by adding favorite 
 def add_favorite(db, token, favorites, selection):
@@ -16,11 +19,12 @@ def add_favorite(db, token, favorites, selection):
     if selection not in favorites:
         # add to list
         favorites.append(selection)
-    db.child('user').child(token).update({'favorites':favorites})
-    
+    db.child('user').child(token).update({'favorites': favorites})
+
+
 # update database by removing favorite
 def remove_favorite(db, token, favorites, selection):
-        # in favorite list AND  not checked
+    # in favorite list AND  not checked
     if selection in favorites:
         # remove from list
         favorites.remove(selection)
--- website/functions/user.py
+++ website/functions/user.py
@@ -4,25 +4,33 @@ import pyrebase
 from flask_session import Session
 
 
+# TODO: improve, some functions could be argued dont belong in this class
 class UserData:
+    """This object will handle part of the user data management, transformation and interaction with firebase"""
     def __init__(self):
         self.data = {
 
             'favorites': '',
             'meal_plan': ''
         }
+        self.email = ''
+        self.password = ''
         self.recipe_list = {}
         self.title_list = {}
         self.links = {}
         self.user = {}
 
     # --------------------------Data Minipulation---------------------------#
-
-    # turns recipies stored in user database into a dictionary to display as links
-    def user_recipies_to_links(self, db, token, list):
+    def user_recipies_to_links(self, db, token, attribute):
+        """turns recipies stored in user database into a dictionary to display as links on html
+        :param db reference to the database (firebase real time database)
+        :param token user token, used to read right user in database
+        :param attribute which attribute of the user to list
+        :returns List of links
+        """
         self.links = {}
         self.user = db.child('user').get()
-        for favorite in self.user.val()[token][list]:
+        for favorite in self.user.val()[token][attribute]:
             self.links.update({
                 favorite: {
                     'href': favorite.replace(' ', '+'),
@@ -31,8 +39,11 @@ class UserData:
             })
         return self.links
 
-    # turns recipies stored in recipies into a dictionary to diplay as links
     def recipe_to_links(self, db):
+        """turns recipies stored in database into a dictionary to display as links
+        :param db reference to database
+        :returns List of recipe links
+        """
         self.recipe_list = db.child('Recipes').get()
         self.title_list = {}
 
@@ -44,44 +55,70 @@ class UserData:
             
         return self.title_list
 
-    def list_to_links(self, list):
+    def list_to_links(self, recipe_list):
+        """turns a list of recipies into a list of recipe links
+        :param recipe_list: a string list of recipies
+        :returns A list of recipe links
+        """
         self.title_list = {}
-        for recipe in list:
-            self.title_list.update({recipe : {
+        for recipe in recipe_list:
+            self.title_list.update({recipe: {
                 'href': recipe.replace(' ', '+'),
                 'caption': recipe
             }})
         return self.title_list
-        
 
     # ---------------------------Data Retrieval-----------------------------#
-    # gets data from a selected recipe
     def get_recipe_data(self, db, selection):
+        """gets data from a selected recipe
+        :param db: reference to database
+        :param selection: what recipe
+        :returns selected recipe data
+        """
         self.data = db.child('Recipes').child(selection).get()
         return self.data
 
+    # TODO: not good approach for at full scale
     def get_recipes(self, db):
+        """gets recipe (titles) in database to display
+        :param db: reference to database
+        :returns List of available recipies
+        """
         self.data = db.child('Recipes').get()
         return self.data
 
-    # gets user data
     def get_user_data(self, db):
+        """gets user data
+        WARNING: exploitable approach if not used carefully (not that we store any important info on this database)
+        Keyword arguments:
+        :param db: reference to db
+        :returns List of user IDs
+        """
         self.user = db.child('user').get()
         return self.user
 
-    # gets login/signup form data from webpage
+    #
     def login_info(self, form):
+        """ gets login/signup form data from webpage, meant to simplify user management, not secure.
+        :param form: reference to data that the form tag returns from HTML
+        """
         self.email = form['email']
         self.password = form['pass']
 
     # ---------------------------User Management---------------------------- #
-    # creates user on authenticator and pushes user into database for persistant storage
+
     def create_user(self, auth, db):
+        """ creates user on authenticator and pushes user into database for persistent storage
+        :param auth: reference to authenticator
+        :param db: reference to database
+        """
         self.user_token = auth.create_user_with_email_and_password(self.email, self.password)
         db.child('user').child(self.user_token['localId']).update(self.data)
 
-    # logs user in
     def login(self, auth):
+        """ logs user in
+        :param auth: reference to authenticator
+        """
         self.user_token = auth.sign_in_with_email_and_password(self.email, self.password)
 
     def logoff(self):
